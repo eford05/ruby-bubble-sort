77a761c95914bd2559665bb5be9ba8c2b96aceac
Erica Ford <erica.ford0205@gmail.com>
2024-07-27 13:28:32 -0500

    Finished project, added the nested iteration so that all possible values will swap in necessary

--- bubble-sort.rb
+++ bubble-sort.rb
@@ -2,12 +2,23 @@ arr = [4, 3, 78, 2, 0, 2]
 
 # Testing first pass of sort concept
 def bubble_sort (arr)
-  arr.each_with_index {|val, idx|
-  if idx < arr.length - 1
-    #Parallel Assignment
-    if (arr[idx] > arr[idx + 1])
-      arr[idx], arr[idx + 1] = arr[idx + 1], arr[idx]
+  arr.each_with_index {|val, i|
+  # Iterate to this point
+  if i < arr.length - 1
+
+    # Nested Iteration to compare for all possible values
+    arr.each_with_index {|val, j|
+    # Iterate to this point
+    if j < (arr.length - i - 1)
+      
+      # Swap if condition met
+      if arr[j] > arr[j + 1]
+        #Parallel Assignment -> x,y = y,x
+        arr[j], arr[j + 1] = arr[j + 1], arr[j]
+      end
     end
+    }
+    
   else
     break
   end
